2d84ac25544472386855cb69db9ddae2be5f3839
nzr61 <54835821+nzr61@users.noreply.github.com>
2019-10-11 04:28:53 +0300

    Add files via upload

---
+++ KHMEET/js/registration.js
@@ -0,0 +1,27 @@
+$(".close").click(
+function()
+	{
+	var a=document.getElementById('window_a')
+	$(a).css({"display" : "none"});	
+	var w=document.getElementById('window_r')
+	$(w).css({"display" : "none"});	
+	var g=document.getElementById('gray')
+	$(g).css({"display" : "none"}); 		
+	}	
+	);
+$("#reg").click(function()
+	{
+	var w=document.getElementById('window_r')
+	$(w).css({"display" : "block"});	
+	var g=document.getElementById('gray')
+	$(g).css({"display" : "block"}); 		
+	}	
+	);
+$("#aut").click(function()
+	{
+	var w=document.getElementById('window_a')
+	$(w).css({"display" : "block"});	
+	var g=document.getElementById('gray')
+	$(g).css({"display" : "block"}); 		
+	}	
+	);
